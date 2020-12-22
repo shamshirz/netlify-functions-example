1c29a93c242eb408361b75cd00724322393477af
Aaron Votre <avotre422@gmail.com>
2020-12-22 15:12:40 -0500

    change return type of cached fxn

--- src/lambda/cached.js
+++ src/lambda/cached.js
@@ -1,8 +1,9 @@
 exports.handler = async (event, context) => {
   // 86,400 = seconds in 24 hours. Cache result for up to 24 hours in Netlify proxy
+  const dateString = new Date().toDateString
   return {
     statusCode: 200,
     headers: { 'Cache-Control': 'max-age=86400, public', },
-    body: new Date().toDateString
+    body: dateString
   };
 };
