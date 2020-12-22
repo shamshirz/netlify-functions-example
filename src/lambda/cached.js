02709a03279b1ebf7383721174795d905ebaa14d
Aaron Votre <avotre422@gmail.com>
2020-12-22 15:18:02 -0500

    try another date approach

--- src/lambda/cached.js
+++ src/lambda/cached.js
@@ -1,9 +1,20 @@
 exports.handler = async (event, context) => {
   // 86,400 = seconds in 24 hours. Cache result for up to 24 hours in Netlify proxy
-  const dateString = new Date().toDateString
+
+  // current timestamp in milliseconds
+  let ts = Date.now();
+
+  let date_ob = new Date(ts);
+  let date = date_ob.getDate();
+  let month = date_ob.getMonth() + 1;
+  let year = date_ob.getFullYear();
+
+  // prints date & time in YYYY-MM-DD format
+  console.log(year + "-" + month + "-" + date);
+
   return {
     statusCode: 200,
     headers: { 'Cache-Control': 'max-age=86400, public', },
-    body: dateString
+    body: year + "-" + month + "-" + date
   };
 };
